e5889f79c198b2ed3b6d5eb00b0a24084c1ba550
Lou Jacob <lou.jacob@loujacob.com>
2025-04-19 12:35:50 -0600

    Clean slate: remove all tracked legacy files

--- podcast_trends/storage.py
+++
@@ -1,9 +0,0 @@
-import json
-
-def save_to_json(records, path):
-    with open(path, "w") as f:
-        json.dump(records, f, indent=2)
-
-def load_from_json(path):
-    with open(path, "r") as f:
-        return json.load(f)
